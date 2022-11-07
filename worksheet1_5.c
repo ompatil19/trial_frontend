d593bf38df323f438504563454302e1e4b2bfb85
ompatil19 <90125237+ompatil19@users.noreply.github.com>
2022-11-07 21:36:45 +0530

    Add files via upload

---
+++ worksheet1_5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int a,b;
+    while(1){
+        scanf("%d",&a);
+        if(a==-1){
+            break;
+        }
+        scanf("%d",&b);
+        if(a!=b){
+            printf("1");
+            break;
+        }
+    }
+    if(a==-1)
+    printf("0");
+    return 0;
+}
